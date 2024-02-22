e914a03fd4665301ccf5d129cdd1c9e424ebbd47
Tushar Gandhi <gandhitushar509@gamail.com>
2024-02-22 11:17:08 +0530

    hello_world

---
+++ helloworld/app/blog/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>blog page</h1>
+    </div>
+  )
+}
---
+++ helloworld/app/dashboard/settings/password/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Pass() {
+  return (
+    <div>
+      <h1>password Page</h1>
+    </div>
+  )
+}
