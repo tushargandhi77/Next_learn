88e829e2ff73020ac45e38520e8573ed2dcccdc8
Tushar Gandhi <gandhitushar509@gamail.com>
2024-02-23 17:08:26 +0530

    Linking and navigation

--- helloworld/app/page.js
+++ helloworld/app/page.js
@@ -1,9 +1,26 @@
-import React from 'react'
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello World Main Page</h1>
-    </div>
+// Option 1
+// import Link from 'next/link'
+
+// export default function Page(){
+//   return <Link href="/dashboard">Dashboard</Link>
+// }
+
+
+// Option 2
+'use client'
+
+import { useRouter } from "next/navigation"
+
+export default function HOme(){
+  const router = useRouter()
+  return(
+    <>
+    <button type="button" onClick={()=>router.push('/dashboard')}>
+      DashBoard
+    </button>
+    </>
   )
 }
+
+
