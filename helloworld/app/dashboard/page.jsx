50b4303472c572ca141b82e2e0c69d1ace86e7c0
Tushar Gandhi <gandhitushar509@gamail.com>
2024-02-24 23:29:46 +0530

    loading

--- helloworld/app/dashboard/page.jsx
+++ helloworld/app/dashboard/page.jsx
@@ -9,3 +9,19 @@ export default function Dash() {
     </div>
   )
 }
+
+// import { Suspense } from 'react'
+// import { PostFeed, Weather } from './Components'
+ 
+// export default function Posts() {
+//   return (
+//     <section>
+//       <Suspense fallback={<p>Loading feed...</p>}>
+//         <PostFeed />
+//       </Suspense>
+//       <Suspense fallback={<p>Loading weather...</p>}>
+//         <Weather />
+//       </Suspense>
+//     </section>
+//   )
+// }
--- helloworld/app/page.js
+++ helloworld/app/page.js
@@ -11,16 +11,34 @@
 'use client'
 
 import { useRouter } from "next/navigation"
+import { Links } from './components/links'
 
-export default function HOme(){
+export default function Home(){
   const router = useRouter()
   return(
     <>
+    <Links/>
     <button type="button" onClick={()=>router.push('/dashboard')}>
       DashBoard
     </button>
     </>
   )
 }
+
+// // option 3
+// 'use client'
+ 
+// import { useState } from 'react'
+ 
+// export default function Counter() {
+//   const [count, setCount] = useState(0)
+ 
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>Click me</button>
+//     </div>
+//   )
+// }
 
 
